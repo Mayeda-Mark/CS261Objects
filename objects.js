f0522832b0dd6069020798c5568e34f5fe8b8338
Mayeda-Mark <markrmayeda@gmail.com>
2018-10-05 13:15:57 -0700

    fixed the issue

--- objects.js
+++ objects.js
@@ -5,19 +5,15 @@ function Team(name, wins, losses) {
 	this.wins = wins;
 	this.losses = losses;
 }
-
 Team.prototype.players = 12;
 
-function createTeam(name, wins, losses) {
-	var name = getElementById('name').value;
-	var wins = getElementById('wins').value;
-	var losses = getElementById('losses').value;
+function createTeam() {
+	var name = document.getElementById('name').value;
+	var wins = document.getElementById('wins').value;
+	var losses = document.getElementById('losses').value;
 	var team1 = new Team(name, wins, losses);
-	var txt = "The " + team1.name + "has a win/loss record of "
+	var txt = "The " + team1.name + " has a win/loss record of "
 	+ team1.wins + "/" + team1.losses;
-	console.log(team1.name);
-	console.log(team1.wins);
-	console.log(team1.losses);
 	//document.getElementById(name).value = "";
 	//document.getElementById(wins).value = "";
 	//document.getElementById(losses).value = "";
